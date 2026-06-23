3a76a4e7219fdcf82a96f98889072f45c5bfc255
principalMalinoski <gmalinoskiamorim@gmail.com>
2026-06-23 17:19:43 -0300

    segundo commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Login from "./components/login";
+
+function App (){
+  return (
+    <>
+     <Header titulo="Lanchonete Xpress"/>
+     <Header subtitulo="O Melhor da Região"/>
+
+<Login />
+    </>
+
+  )
+}
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,19 @@
+import "./Login.css";
+
+function Login() {
+    return (
+      <>
+        <h2>Login</h2>
+  
+        <label>Usuário</label>
+        <input type="text" />
+  
+        <label>Senha</label>
+        <input type="password" />
+  
+        <button>Entrar</button>
+      </>
+    );
+  }
+  
+  export default Login;
